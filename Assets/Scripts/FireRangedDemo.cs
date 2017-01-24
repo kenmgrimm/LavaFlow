fe7e886610d0bebb8235428b2d7a0f2a3b54b8b3
Ken Grimm <kenmgrimm@gmail.com>
2017-01-23 21:37:58 -0700

    Working import of Lava Flowing Shader

---
+++ Assets/Scripts/FireRangedDemo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FireRangedDemo : MonoBehaviour {
+	[SerializeField]
+	private List<GameObject> actions = new List<GameObject>();
+
+	void Update() {
+
+	}
+
+}
