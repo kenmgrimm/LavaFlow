ff9d9bd639fc15399766e3b86404e79bfa0a6a18
Ken Grimm <kenmgrimm@gmail.com>
2017-02-09 16:43:50 -0700

    Modified fire ranged to have a throat, 2 event points in the Animator for MouthOpen and Spit, and a simple fireball emitting on spit

---
+++ Assets/Scripts/FireRanged.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FireRanged : MonoBehaviour {
+	private Animator animator;
+	public Transform throat;
+	
+	public GameObject fireball;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void MouthOpen () {
+		Debug.Log("MouthOpen");
+	}
+	
+	void Spit () {
+		Debug.Log("Spit");
+		Debug.Log(throat.position);
+		var effect = Instantiate(fireball, throat);
+		effect.transform.localPosition = Vector3.zero;
+		effect.transform.rotation = throat.rotation;
+		effect.SetActive(true);
+		// effect.transform.position = throat.position;	
+	}
+}
