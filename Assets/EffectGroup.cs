2f414fa8126b80acb85883064712de5b66d88aae
Ken Grimm <kenmgrimm@gmail.com>
2017-02-08 17:16:37 -0700

    Beginnings of working effects playground UI

---
+++ Assets/EffectGroup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectGroup : MonoBehaviour {
+  public List<GameObject> effects;
+	private int currentIndex = 0;
+	
+	public void Start() {
+		if(effects == null || effects.Count == 0) {
+			Debug.LogError("No effects set, aborting");
+			gameObject.SetActive(false);
+		}
+		
+		GameObject.Find("Demo Controller").GetComponent<DemoController>().RegisterEffectGroup(this);
+	}
+
+	public void OnChangePlay(bool start) {
+		Debug.Log("OnChangePlay: " + start);
+
+		CurrentEvent().SetActive(start);
+	}
+	
+	public void OnAdvanceEffect(int count) {
+		Debug.Log("OnAdvanceEffect: " + count);
+		
+		if(!CurrentEvent().activeSelf) {
+			return;
+		}
+
+		CurrentEvent().SetActive(false);
+		
+		currentIndex += count;
+		
+		if (currentIndex < 0) {
+			currentIndex = effects.Count - 1;
+		}
+		else if (currentIndex >= effects.Count) {
+			currentIndex = 0;
+		}
+		
+		CurrentEvent().SetActive(true);
+	}
+	
+	private GameObject CurrentEvent() {
+		return effects[currentIndex];
+	}
+}
---
+++ Assets/Scripts/DemoController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DemoController : MonoBehaviour {
+  public Button advanceLeft;
+  public Button advanceRight;
+  
+  void Start() {
+    if(advanceLeft == null || advanceRight == null) {
+      Debug.LogError("Not all elements set in editor, aborting");
+      gameObject.SetActive(false);
+    }
+  }
+  
+  public void RegisterEffectGroup(EffectGroup effectGroup) {
+    advanceLeft.onClick.AddListener(() => effectGroup.OnAdvanceEffect(-1));
+    advanceRight.onClick.AddListener(() => effectGroup.OnAdvanceEffect(1));
+  }
+}
